1eeccb2935d8a8fa34c19cd5ae8c31ac77ff9dc5
Alex Lossberg <50721703+LDwhoRU@users.noreply.github.com>
2020-09-30 00:16:31 +1000

    Implemented matrix operation into Spherical holder func

--- suspension/PickupLocations.py
+++ suspension/PickupLocations.py
@@ -1,4 +1,5 @@
 from sympy import *
+import numpy as np
 
 class PickupLocations:
 
@@ -13,7 +14,25 @@ class PickupLocations:
             raise Exception("Error: Position must be top or bottom")
 
     def calculate_spherical_holders(self, interval, min, max):
-        
+        A = np.matrix([
+            [1,0,0,-(self.rearChassis.get_x()-self.frontChassis.get_x())],
+            [0,1,0,-(self.rearChassis.get_y()-self.frontChassis.get_y())],
+            [0,0,1,-(self.rearChassis.get_z()-self.frontChassis.get_z())],
+            [(self.rearChassis.get_x()-self.frontChassis.get_x()), (self.rearChassis.get_y()-self.frontChassis.get_y()), (self.rearChassis.get_z()-self.frontChassis.get_z()), -1]
+        ])
+
+        b = np.transpose(np.matrix([
+            [self.frontChassis.get_x(), self.frontChassis.get_y(), self.frontChassis.get_z(), self.sphericalHolder.get_x()*(self.rearChassis.get_x()-self.frontChassis.get_x()) + self.sphericalHolder.get_y()*(self.rearChassis.get_y()-self.frontChassis.get_y()) + self.sphericalHolder.get_z()*(self.rearChassis.get_z()-self.frontChassis.get_z()) ]
+        ]))
+
+        sol = np.linalg.solve(A,b)
+        x = sol[0].tolist()[0][0]
+        y = sol[1].tolist()[0][0]
+        z = sol[2].tolist()[0][0]
+
+        print(x)
+        print(y)
+        print(z)
         pass
 
     def calc_SH(self, interval, min, max):
